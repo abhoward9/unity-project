a13ff76b62e3506beda4c64cdc29a808d497e104
Andrew <abhoward9@icloud.com>
2025-08-06 20:37:56 -0700

    initial commit

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+}
+
